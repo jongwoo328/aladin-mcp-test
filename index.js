37ac2e83d153db44b69273d697bb8b2f8611c230
Lee Jongwoo <jongwoo328@gmail.com>
2025-05-07 14:44:25 +0900

    bin 설정

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import { createRequire } from 'node:module';
 import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js';
 import { StdioServerTransport } from '@modelcontextprotocol/sdk/server/stdio.js';
